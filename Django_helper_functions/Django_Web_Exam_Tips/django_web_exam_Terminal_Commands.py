cdd68e73644e978ba9322731b1ce2f128f8cea32
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-06-01 14:01:07 +0300

    useful terminal commands (managing project & apps)

---
+++ Django_helper_functions/Django_Web_Exam_Tips/django_web_exam_Terminal_Commands.py
@@ -0,0 +1,16 @@
+django-admin startproject myproject
+
+python manage.py startapp web
+
+python manage.py runserver
+
+python manage.py makemigrations
+python manage.py migrate
+
+startapp web #manage.py (tools -> run manage.py tasks)
+
+
+venv\Scripts\activate
+
+pip install psycopg2-binary
+pip freeze > requirements.txt
